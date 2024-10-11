efdff877ca40512afefaa419af464e5987abae13
aimrrs <aimrrs404@gmail.com>
2024-10-11 22:15:25 +0530

    All txt feature are removed for db implementation.

--- dbbro.py
+++ dbbro.py
@@ -6,10 +6,16 @@ myconn = mysql.connector.connect(host = "localhost",
                                  database = "lab1")
 cur = myconn.cursor()
 
+def ERC(command):
+    cur.execute(command)
+    result = cur.fetchall()
+    return result
 
-
-
-
+def EWC(command):
+    cur.execute(command)
+    cur.commit()
+    result = f"{cur.rowcount} Modified."
+    return result
 
 
 myconn.close()
--- main.py
+++ main.py
@@ -1,47 +1,17 @@
 from os import path
 import time
+from tabulate import tabulate
+from dbbro import ERC
 
 class Register:
     def __init__(self):
-        self.currentTime = time.strftime("%H:%M:%S")
-        self.todayDate = time.strftime("%Y-%m-%d")
-        self.inCount = 0
-        self.outCount = 0
-        self.name = None
-        self.nodeNumber = None
-        self.rollNumber = None
+        pass
 
     def entry(self, name, nodeNumber, rollNumber):
-        self.name, self.nodeNumber, self.rollNumber = name, nodeNumber, rollNumber
-
-        if path.exists(f"{self.todayDate}.txt"):
-            with open(f"{self.todayDate}.txt", "a+") as entryFileObject:
-                entryFileObject.write(f"{name} : {nodeNumber} : {rollNumber} : IN - {self.currentTime} : OUT - (not yet)\n")
-        else:
-            with open(f"{self.todayDate}.txt", "w") as fotemp:
-                pass
-
-        self.inCount += 1
         return
 
     def exitLab(self):
-        if path.exists(f"{self.todayDate}.txt"):
-            with open(f"{self.todayDate}.txt", "a+") as ELFileObject:
-                ELFileObject.write(f"{self.name} : {self.nodeNumber} : {self.rollNumber} : IN - {self.currentTime} : OUT - {self.currentTime}\n")
-        else:
-            with open(f"{self.todayDate}.txt", "w") as fotemp:
-                pass
-
-        self.outCount += 1
         return
 
     def inMates(self):
-        inMatesList = []
-        if path.exists(f"{self.todayDate}.txt"):
-            with open(f"{self.todayDate}.txt", "r") as inMatesFileObject:
-                if (len(inMatesFileObject.readlines()) != 0) and (self.inCount - self.outCount != 0):
-                    entries = inMatesFileObject.readlines()
-                    for entri in entries:
-                        if ": IN - " in entri:
-                            inMatesList.append(entri)
-        return inMatesList
+        return
